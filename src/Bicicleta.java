c008f5a491473086b7439820eeae3083e7f1528f
Veridiana Melo <veridianadasilvameloxxi@gmail.com>
2015-02-11 20:10:34 -0200

    [Alterações do exercícios]

--- src/Bicicleta.java
+++ src/Bicicleta.java
@@ -1,4 +1,13 @@
+//acessar site git / criar novo repositorio o git fornece endereço/ passo a passo git initial, git add -A adicionar todos os arquivos
 
+// git commit para fazer todas as  alterações.
+// git push o primeiro push git push -u 
+
+//git init
+//git add -A
+//git commit -m "first commit"
+//git remote add origin https://github.com/veridianaMelo/projetos.git
+//git push -u origin master
 public class Bicicleta {
 
 }
